c62d0d4cdd2b67fbe32497c1ac1d15f8e5c78f1a
semel84mukan <152295085+semel84mukan@users.noreply.github.com>
2024-03-19 14:42:49 +0300

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,49 @@
+}
+    const data = generateRandomData();
+    });
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    const data = [];
+        console.log("Random Number: " + item);
+    }
+function generateRandomData() {
+    data.forEach(item => {
+
+
+}
+    for (let i = 0; i < 10; i++) {
+main();
+    return data;
+function main() {    data.forEach(item => {
+
+    const data = [];
+
+main();
+    const data = generateRandomData();
+    }
+function main() {
+        console.log("Random Number: " + item);
+    });
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    for (let i = 0; i < 10; i++) {
+    return data;
+function generateRandomData() {
+}
+function main() {
+main();
+
+}
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+
+function generateRandomData() {
+    }
+    const data = generateRandomData();
+    });
+    return data;
+        console.log("Random Number: " + item);
+    data.forEach(item => {
